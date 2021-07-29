167331848617d3797f0b45c3b5f4757a125f380b
Shashvat Mohane <shashvat.mohane@gmail.com>
2021-07-29 12:38:12 +0530

    Multicall v2 support added

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -25,7 +25,7 @@ import ERC20_ABI from '../constants/abis/erc20.json'
 import { MIGRATOR_ABI, MIGRATOR_ADDRESS } from '../constants/abis/migrator'
 import UNISOCKS_ABI from '../constants/abis/unisocks.json'
 import WETH_ABI from '../constants/abis/weth.json'
-import { MULTICALL_ABI, MULTICALL_NETWORKS } from '../constants/multicall'
+import { MULTICALLV2_ABI, MULTICALL_ABI, MULTICALL_NETWORKS, MULTICALL_V2_NETWORKS } from '../constants/multicall'
 import { V1_EXCHANGE_ABI, V1_FACTORY_ABI, V1_FACTORY_ADDRESSES } from '../constants/v1'
 import { getContract } from '../utils'
 import { useActiveWeb3React } from './index'
@@ -126,6 +126,10 @@ export function usePairContract(pairAddress?: string, withSignerIfPossible?: boo
 export function useMulticallContract(): Contract | null {
   const { chainId } = useActiveWeb3React()
   return useContract(chainId && MULTICALL_NETWORKS[chainId], MULTICALL_ABI, false)
+}
+export function useMulticallV2Contract(): Contract | null {
+  const { chainId } = useActiveWeb3React()
+  return useContract(chainId && MULTICALL_V2_NETWORKS[chainId], MULTICALLV2_ABI, false)
 }
 
 export function useMerkleDistributorContract(): Contract | null {
