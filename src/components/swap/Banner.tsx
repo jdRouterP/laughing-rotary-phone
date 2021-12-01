d804dc98aae13a5a94c18ae9118d8cc9d17d3c95
Ganesh <sivaganesh.bhagi@gmail.com>
2021-12-01 19:09:30 +0530

    update dfyn burn banner

--- src/components/swap/Banner.tsx
+++ src/components/swap/Banner.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 import BackgroundBanner from '../../assets/images/burnDfyn.png'
 // import { HEADER_ACCESS } from 'constants/networks'
-import { Countdown } from 'components/CountDownSwap'
 import styled from 'styled-components'
 import { ExternalLink, TYPE } from 'theme'
 // import BURNDFYN from '../../assets/images/burn.png'
-
+import { OpenInNew } from '@material-ui/icons'
 
 const BannerBox = styled.div`
   max-width: 420px;
@@ -23,21 +22,37 @@ const InnerContent = styled.div`
   position: absolute;
 `
 
+const CustomExternalLink = styled(ExternalLink)`
+    display: inline-block;
+    margin-right: 30px;
+    margin-top: 20px;
+`
+
 export default function Banner() {
     return (
         <BannerBox>
             <img src={BackgroundBanner} alt={'banner'} style={{ width: "100%"}} />
             <InnerContent>
-            <TYPE.white fontSize="15px" fontWeight="500">50 Million DFYN tokens will be burned in</TYPE.white>
-            <Countdown exactEnd={new Date(1638363600000)} start={new Date()} />
-            <ExternalLink
+            <TYPE.white fontSize="20px" fontWeight="500">50 Million DFYN tokens burnt</TYPE.white>
+            
+            <CustomExternalLink
+                style={{ color: 'white', textDecoration: 'underline'}}
+                href="https://etherscan.io/tx/0x2285411b4ece226e5b68cff956d3e293c678037ce32c7f4d427c30f460cffad7"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <TYPE.white fontSize="14px" marginBottom="15px" marginTop="5px" style={{cursor: "pointer"}}>View transaction 
+                <OpenInNew style={{ fontSize: "14px", marginLeft: '2px' }}/></TYPE.white>
+            </CustomExternalLink>
+
+            <CustomExternalLink
                 style={{ color: 'white', textDecoration: 'underline'}}
                 href="https://dfyn-network.medium.com/major-tokenomics-changes-in-dfyn-token-934fda5444f1"
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                <TYPE.white fontSize="12px" marginTop="5px" style={{cursor: "pointer"}}>Learn More</TYPE.white>
-            </ExternalLink>
+                <TYPE.white fontSize="14px" marginTop="5px" style={{cursor: "pointer"}}>Learn More</TYPE.white>
+            </CustomExternalLink>
             </InnerContent>
         </BannerBox>
     )
