4feb0e683521bfc4ffe36641dd579765dca6c50b
hacker14398 <ankush8233346877@gmail.com>
2021-12-15 17:49:03 +0530

    Increase Gas Limit

--- src/components/multiRewardLaunchFarms/ClaimRewardModal.tsx
+++ src/components/multiRewardLaunchFarms/ClaimRewardModal.tsx
@@ -46,7 +46,7 @@ export default function ClaimRewardModal({ isOpen, onDismiss, stakingInfo }: Sta
     if (stakingContract && stakingInfo?.stakedAmount) {
       setAttempting(true)
       await stakingContract
-        .getReward({ gasLimit: 400000 })
+        .getReward({ gasLimit: 500000 })
         .then((response: TransactionResponse) => {
           addTransaction(response, {
             summary: `Claim accumulated rewards`
