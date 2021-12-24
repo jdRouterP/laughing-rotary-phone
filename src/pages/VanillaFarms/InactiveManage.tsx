d21ae0c52480056eb29fbd48a600c16d84233a93
Shashvat Mohane <shashvat.mohane@gmail.com>
2021-12-24 11:59:48 +0530

    fix: decimal issue fixed

--- src/pages/VanillaFarms/InactiveManage.tsx
+++ src/pages/VanillaFarms/InactiveManage.tsx
@@ -134,7 +134,7 @@ export default function Manage({
     )
   }
 
-  const countUpAmount = stakingInfo?.earnedAmount?.toFixed(6) ?? '0'
+  const countUpAmount = stakingInfo?.earnedAmount?.toFixed(4) ?? '0'
   const countUpAmountPrevious = usePrevious(countUpAmount) ?? '0'
 
   // get the USD value of staked WETH
