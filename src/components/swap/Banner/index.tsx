a1405c6f6f469d43692a80c196799f9672ac3389
jdRouterP <91524972+jdRouterP@users.noreply.github.com>
2021-12-28 20:15:45 +0530

    Configured husky to format code before commit (#48)
    * chore: configure prettierr
    
    * chore: configure husky to run prettier before commit
    
    * demo commit
    
    Co-authored-by: Joydeep Singha <67229156+Jds-23@users.noreply.github.com>

--- src/components/swap/Banner/index.tsx
+++ src/components/swap/Banner/index.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import Banner from "./Banner";
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import Banner from './Banner'
+
 export const BannerWrapper = styled.div`
   transition: transform 700ms ease-in-out;
   position: relative;
@@ -9,41 +10,36 @@ export const BannerWrapper = styled.div`
   width: 100%;
   max-width: 420px;
   margin: 0 auto;
-`;
+`
 interface CarouselContent {
-  description: string;
-  image: string;
-  value: number;
-  index: number;
-  length: number;
-  viewTransaction: string;
-  learnText: string;
-  transaction: string;
-  learnMore: string;
+  description: string
+  image: string
+  value: number
+  index: number
+  length: number
+  viewTransaction: string
+  learnText: string
+  transaction: string
+  learnMore: string
 }
 const waitTime = 7000
 const BannerCarousel = () => {
-  const [currentCarousel, setCurrentCarousel] = useState(0);
-  const [carouselContent, setCarouselContent] = useState<
-    | undefined
-    | CarouselContent[]
-  >();
+  const [currentCarousel, setCurrentCarousel] = useState(0)
+  const [carouselContent, setCarouselContent] = useState<undefined | CarouselContent[]>()
   useEffect(() => {
     const timeout2 = setTimeout(() => {
-      setCurrentCarousel((prev) => prev + 1);
-    }, waitTime);
-    return () => clearTimeout(timeout2);
-  }, [currentCarousel]);
+      setCurrentCarousel((prev) => prev + 1)
+    }, waitTime)
+    return () => clearTimeout(timeout2)
+  }, [currentCarousel])
   useEffect(() => {
-    getBannerData();
-  }, []);
+    getBannerData()
+  }, [])
   const getBannerData = async () => {
-    fetch(
-      "https://raw.githubusercontent.com/dfyn/banner-data/master/banner-content/index.json"
-    )
+    fetch('https://raw.githubusercontent.com/dfyn/banner-data/master/banner-content/index.json')
       .then((response) => response.json())
-      .then((data) => setCarouselContent(data));
-  };
+      .then((data) => setCarouselContent(data))
+  }
   return (
     <BannerWrapper>
       {carouselContent?.map(({ image, description, viewTransaction, learnText, transaction, learnMore }, index) => {
@@ -60,10 +56,10 @@ const BannerCarousel = () => {
             transaction={transaction}
             learnMore={learnMore}
           />
-        );
+        )
       })}
     </BannerWrapper>
-  );
-};
+  )
+}
 
-export default BannerCarousel;
+export default BannerCarousel
